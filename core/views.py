aa53a1979a683f0d64a0dd5ac7078d392ff5dad6
abdulazizovv <aqllibola567@gmail/com>
2026-03-02 09:57:07 +0500

    bog'lanishlar va admin panel customization

--- core/views.py
+++
@@ -1,5 +0,0 @@
-from django.http import HttpResponse
-
-
-def test_view(request):
-    return HttpResponse("<h1>Djangodan salomlar</h1>")
---
+++ main/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+# from main.views import index_view, about_developer
+from . import views
+
+urlpatterns = [
+    path("", views.index_view, name="index"),
+    path("about/", views.about_developer, name="about")
+]
